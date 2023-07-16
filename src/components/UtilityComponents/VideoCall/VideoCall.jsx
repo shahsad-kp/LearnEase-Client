25d82ca905c77af6b198b2a9070e0014fffae96b
shahsad <shahsadkpklr@gmail.com>
2023-07-16 13:26:16 +0530

    fixed bugs and added loading animation in video call

--- src/components/UtilityComponents/VideoCall/VideoCall.jsx
+++ src/components/UtilityComponents/VideoCall/VideoCall.jsx
@@ -1,38 +1,53 @@
 import {useContext, useEffect, useMemo, useRef} from "react";
 import {useSelector} from "react-redux";
 import {videoCallContext} from "../../../store/VideoCallProvider.jsx";
+import ConnectionAnimation from '../../../assets/loading-images/connecting.mp4';
 
 // eslint-disable-next-line react/prop-types
 export const VideoCall = ({name, width, height, isLecturer, userId, className = ''}) => {
     const user = useSelector(state => state.auth.user);
     const videoRef = useRef(null);
-    const {videoConnections, localStream, connectedUsers} = useContext(videoCallContext)
-    const mediaRef = useRef(new MediaStream());
+    const {videoConnections, localStream, connectedUsers, gotLocalStream} = useContext(videoCallContext);
 
+    const connected = useMemo(() => connectedUsers.has(userId), [connectedUsers, userId]);
     const isSelf = useMemo(() => user.id === userId, [user, userId]);
-
+    console.log(connected)
     useEffect(() => {
-        if (isSelf){
-            if (!localStream.current) return;
-            videoRef.current.srcObject = localStream.current;
+        if (isSelf) {
+            if (!gotLocalStream) return;
+            videoRef.current.srcObject = localStream.current
         } else {
-            if (connectedUsers.has(userId)){
+            if (connected) {
                 videoRef.current.srcObject = videoConnections.current.get(userId).remoteStream;
             }
         }
-    }, [connectedUsers, isSelf, localStream, userId, videoConnections]);
+    }, [connected, gotLocalStream, isSelf, localStream, userId, videoConnections]);
 
     return (
         <div
             className={`h-full bg-cover bg-center rounded relative w-[${width}] h-[${height}] ${className}`}
         >
-            <video
-                className={`relative object-cover object-center rounded w-[${width}] h-[${height}]`}
-                muted={isSelf}
-                ref={videoRef}
-                style={{height: '100%', width: '100%'}}
-                autoPlay={true}
-            />
+            {
+                (!connected && !isSelf) ? (
+                    <video
+                        key={`loading-${userId}`}
+                        className={`relative object-cover object-center rounded w-[${width}] h-[${height}]`}
+                        style={{height: '100%', width: '100%'}}
+                        autoPlay={true}
+                        src={ConnectionAnimation}
+                        loop={true}
+                    />
+                ) :
+                    <video
+                        key={`video-${userId}`}
+                        className={`relative object-cover object-center rounded w-[${width}] h-[${height}]`}
+                        muted={isSelf}
+                        ref={videoRef}
+                        style={{height: '100%', width: '100%'}}
+                        autoPlay={true}
+                        loop={true}
+                    />
+            }
             <div className={'absolute left-0 top-0 flex flex-row text-sm'}>
                 <div className={'rounded-tl bg-logo-yellow px-2 font-semibold'}>
                     {isLecturer ? 'Lecturer' : 'Student'}
--- src/store/VideoCallProvider.jsx
+++ src/store/VideoCallProvider.jsx
@@ -3,14 +3,16 @@ import useWebSocket from "react-use-websocket";
 import {useSelector} from "react-redux";
 import {useParams} from "react-router-dom";
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const videoCallContext = createContext(null)
 
 // eslint-disable-next-line react/prop-types
 const VideoCallProvider = ({children}) => {
     const localStream = useRef(null);
-    const [connectedUsers, setConnectedUsers] = useState(new Set());
+    const [gotLocalStream, setGotLocalStream] = useState(false);
     const isCalling = useRef(false);
     const connections = useRef(new Map());
+    const [connectedUsers, setConnectedUsers] = useState(new Set());
     const {roomId} = useParams()
     const loggedInUser = useSelector(state => state.auth.user);
 
@@ -21,62 +23,69 @@ const VideoCallProvider = ({children}) => {
     let videoCallSocket;
 
     useEffect(() => {
-        if (localStream.current) {
-            return;
-        }
-        navigator.mediaDevices.getUserMedia({
-            video: true,
-            audio: true
-        }).then(stream => {
-            localStream.current = stream;
-             connections.current.forEach((userConnection) => {
-                stream.getTracks().forEach(track => {
-                    userConnection.connection.addTrack(track, stream);
+        if (!localStream.current) {
+            navigator.mediaDevices.getUserMedia({
+                "video": true,
+                "audio": true
+            }).then(stream => {
+                connections.current.forEach((userConnection) => {
+                    stream.getTracks().forEach(track => {
+                        userConnection.connection.addTrack(track, stream);
+                    })
                 })
+                localStream.current = stream;
+                setGotLocalStream(true);
+            }).catch(err => {
+                console.log(err);
             })
-        }).catch(err => {
-            console.log(err);
-        })
+        }
 
         return () => {
-            if (localStream.current) {
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+            for (const userConnections in connections.current){
+                userConnections.connection.stop();
+            }
+            if (gotLocalStream) {
                 localStream.current.getTracks().forEach(track => {
                     track.stop();
                 })
             }
         }
-    }, []);
+    }, [gotLocalStream, localStream]);
 
     const createConnection = useCallback((userId) => {
         const remoteStream = new MediaStream();
         const connection = new RTCPeerConnection({
-            iceServers: [
+            "iceServers": [
                 {
-                    urls: 'stun:stun.l.google.com:19302'
+                    "urls": 'stun:stun.l.google.com:19302'
                 }
             ]
         });
         connection.onicecandidate = (event) => {
             if (event.candidate) {
+                console.log('sending ice candidate')
                 videoCallSocket.sendMessage(JSON.stringify({
-                    type: 'ice-candidate',
-                    userId: userId,
-                    candidate: event.candidate
+                    "type": 'ice-candidate',
+                    "userId": userId,
+                    "candidate": event.candidate
                 }))
             }
         }
         connection.ontrack = (event) => {
-
             remoteStream.addTrack(event.track);
         }
         connection.onconnectionstatechange = () => {
+            console.log('connection state changed', connection.connectionState)
             if (connection.connectionState === 'connected') {
+                console.log('rtc connected')
                 setConnectedUsers((prev) => {
                     const newSet = new Set(prev);
                     newSet.add(userId);
                     return newSet;
                 })
             } else if (connection.connectionState === 'disconnected') {
+                console.log('rtc disconnected')
                 setConnectedUsers((prev) => {
                     const newSet = new Set(prev);
                     newSet.delete(userId);
@@ -84,16 +93,16 @@ const VideoCallProvider = ({children}) => {
                 })
             }
         }
-        if (localStream.current) {
+        if (gotLocalStream) {
             localStream.current.getTracks().forEach(track => {
                 connection.addTrack(track, localStream.current);
             })
         }
         return {
-            connection: connection,
-            remoteStream: remoteStream
+            "connection": connection,
+            "remoteStream": remoteStream
         }
-    }, [videoCallSocket]);
+    }, [gotLocalStream, videoCallSocket]);
 
     const onMessage = useCallback((message) => {
         const data = JSON.parse(message.data);
@@ -104,14 +113,14 @@ const VideoCallProvider = ({children}) => {
                 .then(offer => {
                     connection.setLocalDescription(offer).then(() => {
                         videoCallSocket.sendMessage(JSON.stringify({
-                            type: 'offer',
-                            userId: userId,
-                            offer: offer
+                            "type": 'offer',
+                            "userId": userId,
+                            "offer": offer
                         }))
                         connections.current.set(userId, {
-                            userId: userId,
-                            connection: connection,
-                            remoteStream: remoteStream
+                            "userId": userId,
+                            "connection": connection,
+                            "remoteStream": remoteStream
                         })
                     });
                 })
@@ -128,9 +137,9 @@ const VideoCallProvider = ({children}) => {
                         .then(answer => {
                             connection.setLocalDescription(answer).then(() => {
                                 videoCallSocket.sendMessage(JSON.stringify({
-                                    type: 'answer',
-                                    userId: userId,
-                                    answer: answer
+                                    "type": 'answer',
+                                    "userId": userId,
+                                    "answer": answer
                                 }))
                             });
                         })
@@ -142,14 +151,13 @@ const VideoCallProvider = ({children}) => {
                     console.log(err);
                 })
             connections.current.set(userId, {
-                userId: userId,
-                connection: connection,
-                remoteStream: remoteStream
+                "userId": userId,
+                "connection": connection,
+                "remoteStream": remoteStream
             })
         } else if (data.type === 'answer') {
             const userId = data.userId;
-
-            let userConnection =  connections.current.get(userId);
+            let userConnection = connections.current.get(userId);
             if (userConnection === undefined) {
                 return;
             }
@@ -161,7 +169,7 @@ const VideoCallProvider = ({children}) => {
 
         } else if (data.type === 'ice-candidate') {
             const userId = data.userId;
-            let userConnection =  connections.current.get(userId);
+            let userConnection = connections.current.get(userId);
             if (userConnection === undefined) {
                 return;
             }
@@ -174,24 +182,23 @@ const VideoCallProvider = ({children}) => {
     }, [connections, createConnection, videoCallSocket]);
 
     videoCallSocket = useWebSocket(endPoint, {
-        onOpen: () => {
+        "onOpen": () => {
             if (!isCalling.current) {
                 videoCallSocket.sendMessage(JSON.stringify({
-                    type: 'join',
+                    "type": 'join',
                 }))
-
                 isCalling.current = true;
             }
         },
-        onClose: () => {
+        "onClose": () => {
             isCalling.current = false;
         },
-        shouldReconnect: () => true,
-        onMessage: onMessage
+        "shouldReconnect": () => true,
+        "onMessage": onMessage
     });
 
     return (
-        <videoCallContext.Provider value={{localStream, videoConnections: connections, connectedUsers}}>
+        <videoCallContext.Provider value={{localStream, "videoConnections": connections, connectedUsers, gotLocalStream}}>
             {children}
         </videoCallContext.Provider>
     )
