277f61fa7cd1f5b448932d17ca9ac019841e75a6
shahsad <shahsadkpklr@gmail.com>
2023-07-27 10:44:21 +0530

    fixed modal closing when clicking outside

--- src/components/UtilityComponents/Modal/Modal.jsx
+++ src/components/UtilityComponents/Modal/Modal.jsx
@@ -1,11 +1,26 @@
 import {GrFormClose} from "react-icons/gr";
+import {useEffect} from "react";
 
 // eslint-disable-next-line react/prop-types
 export const Modal = ({children, closeFunction, classNames, ...extra}) => {
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            if (!event.target.closest('#modal')) {
+                closeFunction();
+            }
+        };
+
+        window.addEventListener('mousedown', handleOutsideClick);
+
+        return () => {
+            window.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, [closeFunction]);
+    
     return (
         <div className={'fixed w-screen h-screen flex justify-center items-center z-[999] top-0 left-0'}
              style={{backgroundColor: 'rgba(0,0,0,0.8)'}}>
-            <div {...extra} className={'relative w-full m-2 md:m-0 md:max-w-[50%] ' + classNames}>
+            <div id={'modal'} {...extra} className={'relative w-full m-2 md:m-0 md:max-w-[50%] ' + classNames}>
                 <div className={'absolute top-0 right-0 p-2 '}>
                     <GrFormClose color={'white'} className={'cursor-pointer'} onClick={closeFunction}/>
                 </div>
